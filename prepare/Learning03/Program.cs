e04fdbb80d8f5d51d1e46249b701b27931e3d04d
Jacob Bateman <bat18004@byui.edu>
2026-05-18 18:43:44 -0600

    Progress on prep 05

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,19 @@
 using System;
-
-class Program
-{
-    static void Main(string[] args)
+    public class Fraction
     {
-        Console.WriteLine("Hello Learning03 World!");
+     private int _top;
+    private int _bottom;   
+    
+    public Fraction ()
+    {_top = 1;
+    _bottom = 1;}
+
+
+    public Fraction (int numerator)
+    {_top = numerator;
+    _bottom = 1;}
+
+    public Fraction (int numerator, int denominator)
+    {_top = numerator;
+    _bottom = denominator;}
     }
-}
