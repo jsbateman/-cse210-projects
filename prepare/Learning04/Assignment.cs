e9fcaf652ba80f143a40a8316931271c082e62ec
Jacob Bateman <bat18004@byui.edu>
2026-06-01 19:50:20 -0600

    Created files and worked on Learning04

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Assignment
+{
+private string _studentName; 
+private string _topic;
+
+
+public Assignment(string studentName, string topic)
+{
+    _studentName= studentName;
+    _topic = topic;
+
+}
+
+}
---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class MathAssignment: Assignment
+{
+private string _problems; 
+private string _textbookSection;
+public MathAssignment (string _studentName, string _topic, string _textbookSection, string  _problems)
+{}
+}
